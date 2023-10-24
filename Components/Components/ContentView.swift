7a5a9902f816c6c651ecfff93726fbf9699812e6
Vidyadhar Thatte <rockstarvd@gmail.com>
2023-10-24 12:19:29 -0700

    Updated button text color to red in ContentView Uploaded via Trace AI - https://trace.zip/c/156857c5-328a-4c68-9d03-d4e22104ebcc

--- Components/Components/ContentView.swift
+++ Components/Components/ContentView.swift
@@ -6,16 +6,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello")
-            
             // 1. Create a blue button using the design system styles
             Button(action: {
                 // 1.1. Add the action to be performed when the button is tapped
             }) {
                 // 1.2. Set the button text, font, and color
-                Text("Button Text")
+                Text("Button")
                     .font(.system(size: 17, weight: Font.Weight.medium, design: .default))
-                    .foregroundColor(.white)
+                    .foregroundColor(.red)
                 
                 // 1.3. Add padding, background color, and corner radius to style the button
             }
